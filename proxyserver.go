280f4e9694506cd51693f3d0a98d6065e6a03f14
Rafael Idrisov <rafael.idrisov@gmail.com>
2024-05-30 23:17:02 +0300

    redirecting response

--- proxyserver.go
+++ proxyserver.go
@@ -1,1 +1,17 @@
 package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func handleRequest(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func main() {
+	http.HandleFunc("/", handleRequest)
+	fmt.Println("Listening...")
+	log.Fatal("error listening and serve:", http.ListenAndServe(":8080", nil))
+}
