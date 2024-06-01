23c39ca5d8c06dae36c8d6f315fa62330380ea8d
Rafael Idrisov <rafael.idrisov@gmail.com>
2024-06-01 21:04:32 +0300

    Home PC 01.06.2024 21:04

--- proxyserver.go
+++ proxyserver.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"log"
 	"net/http"
+	"os"
 )
 
 func handleRequest(w http.ResponseWriter, r *http.Request) {
@@ -11,17 +13,31 @@ func handleRequest(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		log.Println("Error processing request:", err)
 		w.WriteHeader(http.StatusInternalServerError)
-		_, _ = w.Write([]byte("An internal server error occurred"))
+		_, _ = w.Write([]byte("DefaultClient.Do(): An internal server error occurred"))
 		return
 	}
 	defer redirReq.Body.Close()
 
+	byteBody, err := io.ReadAll(redirReq.Body)
+	if err != nil {
+		log.Println("Error processing request:", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		_, _ = w.Write([]byte("An internal server error occurred"))
+		return
+	}
+
 	w.Header().Set("Status", "Ok")
 	w.Header().Set("Done", "True")
-	// w.Write(redirReq.Body)
+	w.Write(byteBody)
 }
 
 func main() {
+	if len(os.Args) < 1 {
+		fmt.Println("Usage: go run main.go <url>")
+		return
+	}
+	url := os.Args[1]
+
 	http.HandleFunc("/", handleRequest)
 	fmt.Println("Listening...")
 	log.Fatal("error listening and serve:", http.ListenAndServe(":8080", nil))
